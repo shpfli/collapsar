a5107e30bbb0be7f293e27e684a5768a71f56417
zidan.lpf <zidan.lpf@antfin.com>
2018-11-19 00:00:23 +0800

    init

---
+++ src/main/java/com/hubery/collapsar/CollapsarApplication.java
@@ -0,0 +1,12 @@
+package com.hubery.collapsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollapsarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollapsarApplication.class, args);
+	}
+}
